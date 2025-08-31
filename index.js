72b02a5ab6ba0e51d656ac161fddde7f85cee365
Elyyihz <elynne123lima@gmail.com>
2025-08-31 16:40:13 -0300

    Comentando Codigo

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const prompt = require("prompt-sync")();
 
 console.log("--- Calculadora Simples ---");
 
+//Inserindo Números e operador númerico
 let num1 = parseFloat(prompt("Digite o primeiro número: "));
 let operador = prompt("Digite a operação (+, -, *, /): ");
 let num2 = parseFloat(prompt("Digite o segundo número: "));
 
 let resultado;
 
+//Utilizando o Swtch para fazer as operações matematicas
 switch (operador) {
   case "+":
     resultado = num1 + num2;
@@ -19,6 +21,7 @@ switch (operador) {
     resultado = num1 * num2;
     break;
   case "/":
+    //Se o divisor for zero ele imprime uma mensagem avisando que não tem como dividir por zero 
     if (num2 !== 0) {
       resultado = num1 / num2;
     } else {
